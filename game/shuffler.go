03c1240fd3069fbc13c6eca02d0f89f9ed50eee4
eleniums <eleniums@yahoo.com>
2019-12-11 19:33:56 -0800

    Add error to Deal method signature on shuffler

--- game/shuffler.go
+++ game/shuffler.go
@@ -1,5 +1,9 @@
 package game
 
+import (
+	"errors"
+)
+
 type Shuffler struct {
 	deck Deck
 }
@@ -12,7 +16,7 @@ func (s Shuffler) Add(cards ...Card) {
 	// TODO: shuffle cards into the internal deck
 }
 
-func (s Shuffler) Deal() Card {
+func (s Shuffler) Deal() (Card, error) {
 	// TODO: implement deal. Might want to switch to a pointer so we can return nil
-	return NewCard(1, 1)
+	return Card{}, errors.New("there are no cards left to deal")
 }

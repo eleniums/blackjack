d418d1a3f157608b41e144a3496f8691964dbe6d
eleniums <eleniums@yahoo.com>
2019-12-11 22:36:35 -0800

    Add methods for hand

--- game/hand.go
+++ game/hand.go
@@ -15,3 +15,28 @@ func NewHand(cards ...Card) *Hand {
 
 	return &hand
 }
+
+// Count will return the number of cards in the hand.
+func (h *Hand) Count() int {
+	return len(h.cards)
+}
+
+// Cards will return the cards in the hand.
+func (h *Hand) Cards() []Card {
+	return h.cards
+}
+
+// Add a single card to the hand.
+func (h *Hand) Add(card Card) {
+	h.cards = append(h.cards, card)
+}
+
+func (h *Hand) Total() int {
+	// TODO: return total value of cards in hand
+	return 0
+}
+
+func (h *Hand) CanHit() bool {
+	// TODO: return true if not busted
+	return false
+}

d3c2d9743b3d17a733bdace896adb0bb37b27dc8
eleniums <eleniums@yahoo.com>
2019-12-11 20:44:06 -0800

    Add count method and fix add method

--- game/deck.go
+++ game/deck.go
@@ -5,68 +5,76 @@ import (
 	"fmt"
 )
 
-type Deck []Card
+// Deck represents a deck of cards.
+type Deck struct {
+	cards []Card
+}
 
-func NewDeck() Deck {
+// NewDeck creates a new deck of 52 cards. The cards are not in random order.
+func NewDeck() *Deck {
 	var deck Deck
 
 	// add clubs
 	for i := 1; i <= 13; i++ {
 		card := NewCard(SuiteClubs, Rank(i))
-		deck = append(deck, card)
+		deck.cards = append(deck.cards, card)
 	}
 
 	// add spades
 	for i := 1; i <= 13; i++ {
 		card := NewCard(SuiteSpades, Rank(i))
-		deck = append(deck, card)
+		deck.cards = append(deck.cards, card)
 	}
 
 	// add hearts
 	for i := 1; i <= 13; i++ {
 		card := NewCard(SuiteHearts, Rank(i))
-		deck = append(deck, card)
+		deck.cards = append(deck.cards, card)
 	}
 
 	// add diamonds
 	for i := 1; i <= 13; i++ {
 		card := NewCard(SuiteDiamonds, Rank(i))
-		deck = append(deck, card)
+		deck.cards = append(deck.cards, card)
 	}
 
-	return deck
+	return &deck
 }
 
-func (d *Deck) Add(index int, card Card) {
-	deck := *d
-
-	before := deck[0:index]
-	after := deck[index:]
-
-	deck = append(before, card)
-	deck = append(deck, after...)
-
-	*d = deck
+// Count returns the number of cards in the deck.
+func (d *Deck) Count() int {
+	return len(d.cards)
 }
 
+// Add a card to the deck at the specific index.
+func (d *Deck) Add(i int, card Card) {
+	d.cards = append(d.cards, Card{})
+	copy(d.cards[i+1:], d.cards[i:])
+	d.cards[i] = card
+}
+
+// Deal the top card from the deck. This removes the card from the deck.
 func (d *Deck) Deal() (Card, error) {
 	if d.Empty() {
 		return Card{}, errors.New("deck is empty")
 	}
 
-	deck := *d
-	card := deck[0]
-	*d = deck[1:]
+	card := d.cards[0]
+	d.cards = d.cards[1:]
 
 	return card, nil
 }
 
-func (d Deck) Empty() bool {
-	return len(d) == 0
+// Empty returns true if the deck is empty.
+func (d *Deck) Empty() bool {
+	return len(d.cards) == 0
 }
 
-func (d Deck) String() {
-	for _, v := range d {
-		fmt.Printf("%v  ", v)
+// String will return a string representation of the deck.
+func (d *Deck) String() string {
+	var s string
+	for _, v := range d.cards {
+		s += fmt.Sprintf("%v  ", v)
 	}
+	return s
 }

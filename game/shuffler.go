7ba97239649b013527d8d3c01d897424b9c0c064
eleniums <eleniums@yahoo.com>
2019-12-11 18:51:26 -0800

    Stub out shuffler methods

--- game/shuffler.go
+++ game/shuffler.go
@@ -1,2 +1,18 @@
 package game
 
+type Shuffler struct {
+	deck Deck
+}
+
+func NewShuffler() Shuffler {
+	return Shuffler{}
+}
+
+func (s Shuffler) Add(cards ...Cards) {
+	// TODO: shuffle cards into the internal deck
+}
+
+func (s Shuffler) Deal() Card {
+	// TODO: implement deal. Might want to switch to a pointer so we can return nil
+	return NewCard(1, 1)
+}

0256c297ee994026911928c1b52784c2e1700946
eleniums <eleniums@yahoo.com>
2019-12-12 22:46:51 -0800

    Hand returns total value

--- game/hand.go
+++ game/hand.go
@@ -1,5 +1,9 @@
 package game
 
+import (
+	"fmt"
+)
+
 // Hand represents a hand of cards.
 type Hand struct {
 	Cards []Card
@@ -26,12 +30,40 @@ func (h *Hand) Add(card Card) {
 	h.Cards = append(h.Cards, card)
 }
 
+// Total returns the highest hand total without busting, unless busting is unavoidable.
 func (h *Hand) Total() int {
-	// TODO: return total value of cards in hand
-	return 0
+	total := 0
+	numAces := 0
+	for _, v := range h.Cards {
+		if v.Rank() > RankAce && v.Rank() < RankJack {
+			total += int(v.Rank())
+		} else if v.Rank() >= RankJack && v.Rank() <= RankKing {
+			total += 10
+		} else if v.Rank() == RankAce {
+			numAces++
+		}
+	}
+
+	foundAces := 0
+	for _, v := range h.Cards {
+		if v.Rank() == RankAce {
+			foundAces++
+			if foundAces == numAces && total+11 <= 21 {
+				total += 11
+			} else {
+				total++
+			}
+		}
+	}
+
+	return total
 }
 
-func (h *Hand) CanHit() bool {
-	// TODO: return true if not busted
-	return false
+// String will return a string representation of the hand.
+func (h *Hand) String() string {
+	var s string
+	for _, v := range h.Cards {
+		s += fmt.Sprintf("%v  ", v)
+	}
+	return s
 }

b068bfc7bb8f5477f8728ffd907c1b564dedefb0
eleniums <eleniums@yahoo.com>
2019-12-13 17:48:24 -0800

    Add some flags

--- cmd/game/main.go
+++ cmd/game/main.go
@@ -12,6 +12,10 @@ func main() {
 	flag.BoolVar(&game.UseCardSymbols, "use-card-symbols", false, "set to display card symbols instead of text")
 	printCardsTest := flag.Bool("print-cards-test", false, "set to display all cards (for testing purposes)")
 	numDecks := flag.Int("num-decks", 6, "number of shuffled decks to use")
+	// maxDiscard := flag.Int("max-discard", 20, "number of cards allowed in discard pile before shuffling them back in")
+	// startingMoney := flag.Int("starting-money", 100, "amount of money players start with")
+	// minBet := flag.Int("min-bet", 15, "minimum bet allowed")
+	// maxBet := flag.Int("max-bet", 15, "maximum bet allowed")
 	flag.Parse()
 
 	if *printCardsTest {

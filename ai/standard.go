b3ed6f789d889dc9cce7eff4118b25d2066075a4
eleniums <eleniums@yahoo.com>
2019-12-23 18:33:34 -0800

    Remove name from standard ai

--- ai/standard.go
+++ ai/standard.go
@@ -5,17 +5,11 @@ import (
 )
 
 // Standard is an opponent that uses a standard strategy.
-type Standard struct {
-}
+type Standard struct{}
 
 // NewStandard will create a new standard AI.
 func NewStandard() *Standard {
 	return &Standard{}
-}
-
-// Name of player.
-func (ai *Standard) Name() string {
-	return "Larry"
 }
 
 // Action returns the action the player wants to make with his hand.

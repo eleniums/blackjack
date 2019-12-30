62878ba7612e40242c01e0969e95ac5ee8ee967f
eleniums <eleniums@yahoo.com>
2019-12-30 10:15:33 -0800

    Basic implementation for standard
    This is just to have some code in there that works. This needs a real strategy.

--- ai/standard.go
+++ ai/standard.go
@@ -14,12 +14,13 @@ func NewStandard() *Standard {
 
 // Action returns the action the player wants to make with his hand.
 func (ai *Standard) Action(dealer *game.Hand, player *game.Hand) game.Action {
-	// TODO: implement player Action
-	return 0
+	if player.Total() < 16 {
+		return game.ActionHit
+	}
+	return game.ActionStay
 }
 
 // PlaceBet returns the player's bet.
 func (ai *Standard) PlaceBet(minBet, maxBet, totalMoney int) int {
-	// TODO: implement player PlaceBet
-	return 15
+	return minBet
 }

67238e0757b97c2361b8ffd37b6402f905646bd2
eleniums <eleniums@yahoo.com>
2020-01-28 20:47:52 -0800

    Add more hand tests

--- game/hand_test.go
+++ game/hand_test.go
@@ -51,3 +51,159 @@ func Test_Unit_Hand_IsInitialHand(t *testing.T) {
 		})
 	}
 }
+
+func Test_Unit_Hand_CanDouble(t *testing.T) {
+	testCases := []struct {
+		description string
+		cards       []Card
+		expected    bool
+	}{
+		{
+			description: "Two_Cards",
+			cards: []Card{
+				NewCard(SuitHearts, RankJack),
+				NewCard(SuitHearts, RankJack),
+			},
+			expected: true,
+		},
+		{
+			description: "Less_Cards",
+			cards: []Card{
+				NewCard(SuitHearts, RankJack),
+			},
+			expected: false,
+		},
+		{
+			description: "More_Cards",
+			cards: []Card{
+				NewCard(SuitHearts, RankJack),
+				NewCard(SuitHearts, RankJack),
+				NewCard(SuitHearts, RankJack),
+			},
+			expected: false,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.description, func(t *testing.T) {
+			// arrange
+			hand := NewHand(tc.cards...)
+
+			// act
+			result := hand.CanDouble()
+
+			// assert
+			assert.Equal(t, tc.expected, result)
+		})
+	}
+}
+
+func Test_Unit_Hand_CanSplit(t *testing.T) {
+	testCases := []struct {
+		description string
+		cards       []Card
+		expected    bool
+	}{
+		{
+			description: "Two_Matching_Cards",
+			cards: []Card{
+				NewCard(SuitHearts, RankJack),
+				NewCard(SuitHearts, RankJack),
+			},
+			expected: true,
+		},
+		{
+			description: "Two_Mismatched_Cards",
+			cards: []Card{
+				NewCard(SuitHearts, RankJack),
+				NewCard(SuitHearts, RankKing),
+			},
+			expected: false,
+		},
+		{
+			description: "Less_Cards",
+			cards: []Card{
+				NewCard(SuitHearts, RankJack),
+			},
+			expected: false,
+		},
+		{
+			description: "More_Cards",
+			cards: []Card{
+				NewCard(SuitHearts, RankJack),
+				NewCard(SuitHearts, RankJack),
+				NewCard(SuitHearts, RankJack),
+			},
+			expected: false,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.description, func(t *testing.T) {
+			// arrange
+			hand := NewHand(tc.cards...)
+
+			// act
+			result := hand.CanSplit()
+
+			// assert
+			assert.Equal(t, tc.expected, result)
+		})
+	}
+}
+
+func Test_Unit_Hand_IsNatural(t *testing.T) {
+	testCases := []struct {
+		description string
+		cards       []Card
+		expected    bool
+	}{
+		{
+			description: "Natural_Blackjack",
+			cards: []Card{
+				NewCard(SuitHearts, RankJack),
+				NewCard(SuitHearts, RankAce),
+			},
+			expected: true,
+		},
+		{
+			description: "Less_Than_21",
+			cards: []Card{
+				NewCard(SuitHearts, RankJack),
+				NewCard(SuitHearts, RankQueen),
+			},
+			expected: false,
+		},
+		{
+			description: "Greater_Than_21",
+			cards: []Card{
+				NewCard(SuitHearts, RankJack),
+				NewCard(SuitHearts, 5),
+				NewCard(SuitHearts, 7),
+			},
+			expected: false,
+		},
+		{
+			description: "21_With_More_Cards",
+			cards: []Card{
+				NewCard(SuitHearts, RankJack),
+				NewCard(SuitHearts, 5),
+				NewCard(SuitHearts, 6),
+			},
+			expected: false,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.description, func(t *testing.T) {
+			// arrange
+			hand := NewHand(tc.cards...)
+
+			// act
+			result := hand.IsNatural()
+
+			// assert
+			assert.Equal(t, tc.expected, result)
+		})
+	}
+}

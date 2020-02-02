7c6d183afa2b63c35b6e2669bae61c9767955d6a
eleniums <eleniums@yahoo.com>
2020-02-02 14:58:16 -0800

    Add tests for hard and soft hands

--- game/hand_test.go
+++ game/hand_test.go
@@ -207,3 +207,203 @@ func Test_Unit_Hand_IsNatural(t *testing.T) {
 		})
 	}
 }
+
+func Test_Unit_Hand_Soft(t *testing.T) {
+	testCases := []struct {
+		description string
+		cards       []Card
+		expected    bool
+	}{
+		{
+			description: "No_Aces_Less_Than_21",
+			cards: []Card{
+				NewCard(SuitHearts, RankJack),
+				NewCard(SuitHearts, RankKing),
+			},
+			expected: false,
+		},
+		{
+			description: "No_Aces_Equals_21",
+			cards: []Card{
+				NewCard(SuitHearts, RankJack),
+				NewCard(SuitHearts, 5),
+				NewCard(SuitHearts, 6),
+			},
+			expected: false,
+		},
+		{
+			description: "No_Aces_Greater_Than_21",
+			cards: []Card{
+				NewCard(SuitHearts, RankJack),
+				NewCard(SuitHearts, 5),
+				NewCard(SuitHearts, 7),
+			},
+			expected: false,
+		},
+		{
+			description: "Ace_Less_Than_21",
+			cards: []Card{
+				NewCard(SuitHearts, RankAce),
+				NewCard(SuitHearts, 9),
+			},
+			expected: true,
+		},
+		{
+			description: "Ace_Equals_21",
+			cards: []Card{
+				NewCard(SuitHearts, RankAce),
+				NewCard(SuitHearts, RankJack),
+			},
+			expected: true,
+		},
+		{
+			description: "Ace_Greater_Than_21",
+			cards: []Card{
+				NewCard(SuitHearts, RankAce),
+				NewCard(SuitHearts, 4),
+				NewCard(SuitHearts, 7),
+			},
+			expected: false,
+		},
+		{
+			description: "Double_Aces_Less_Than_21",
+			cards: []Card{
+				NewCard(SuitHearts, RankAce),
+				NewCard(SuitHearts, RankAce),
+				NewCard(SuitHearts, 8),
+			},
+			expected: true,
+		},
+		{
+			description: "Double_Aces_Equals_21",
+			cards: []Card{
+				NewCard(SuitHearts, RankAce),
+				NewCard(SuitHearts, RankAce),
+				NewCard(SuitHearts, 9),
+			},
+			expected: true,
+		},
+		{
+			description: "Double_Aces_Greater_Than_21",
+			cards: []Card{
+				NewCard(SuitHearts, RankAce),
+				NewCard(SuitHearts, RankAce),
+				NewCard(SuitHearts, RankJack),
+			},
+			expected: false,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.description, func(t *testing.T) {
+			// arrange
+			hand := NewHand(tc.cards...)
+
+			// act
+			result := hand.Soft()
+
+			// assert
+			assert.Equal(t, tc.expected, result)
+		})
+	}
+}
+
+func Test_Unit_Hand_Hard(t *testing.T) {
+	testCases := []struct {
+		description string
+		cards       []Card
+		expected    bool
+	}{
+		{
+			description: "No_Aces_Less_Than_21",
+			cards: []Card{
+				NewCard(SuitHearts, RankJack),
+				NewCard(SuitHearts, RankKing),
+			},
+			expected: true,
+		},
+		{
+			description: "No_Aces_Equals_21",
+			cards: []Card{
+				NewCard(SuitHearts, RankJack),
+				NewCard(SuitHearts, 5),
+				NewCard(SuitHearts, 6),
+			},
+			expected: true,
+		},
+		{
+			description: "No_Aces_Greater_Than_21",
+			cards: []Card{
+				NewCard(SuitHearts, RankJack),
+				NewCard(SuitHearts, 5),
+				NewCard(SuitHearts, 7),
+			},
+			expected: true,
+		},
+		{
+			description: "Ace_Less_Than_21",
+			cards: []Card{
+				NewCard(SuitHearts, RankAce),
+				NewCard(SuitHearts, 9),
+			},
+			expected: false,
+		},
+		{
+			description: "Ace_Equals_21",
+			cards: []Card{
+				NewCard(SuitHearts, RankAce),
+				NewCard(SuitHearts, RankJack),
+			},
+			expected: false,
+		},
+		{
+			description: "Ace_Greater_Than_21",
+			cards: []Card{
+				NewCard(SuitHearts, RankAce),
+				NewCard(SuitHearts, 4),
+				NewCard(SuitHearts, 7),
+			},
+			expected: true,
+		},
+		{
+			description: "Double_Aces_Less_Than_21",
+			cards: []Card{
+				NewCard(SuitHearts, RankAce),
+				NewCard(SuitHearts, RankAce),
+				NewCard(SuitHearts, 8),
+			},
+			expected: false,
+		},
+		{
+			description: "Double_Aces_Equals_21",
+			cards: []Card{
+				NewCard(SuitHearts, RankAce),
+				NewCard(SuitHearts, RankAce),
+				NewCard(SuitHearts, 9),
+			},
+			expected: false,
+		},
+		{
+			description: "Double_Aces_Greater_Than_21",
+			cards: []Card{
+				NewCard(SuitHearts, RankAce),
+				NewCard(SuitHearts, RankAce),
+				NewCard(SuitHearts, RankJack),
+			},
+			expected: true,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.description, func(t *testing.T) {
+			// arrange
+			hand := NewHand(tc.cards...)
+
+			// act
+			result := hand.Hard()
+
+			// assert
+			assert.Equal(t, tc.expected, result)
+		})
+	}
+}

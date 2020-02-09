e37571f1480c8b9e5dc75adb95f900ba1979cc40
eleniums <eleniums@yahoo.com>
2020-02-09 14:10:26 -0800

    Update coverage script to default to func instead of html

--- scripts/test_coverage.sh
+++ scripts/test_coverage.sh
@@ -2,4 +2,10 @@
 set -e
 
 go test -coverprofile=coverage.out ./...
-go tool cover -html=coverage.out
+
+# show html coverage if specified, otherwise print function coverage
+if [ "$1" == "html" ]; then
+    go tool cover -html=coverage.out
+else
+    go tool cover -func=coverage.out
+fi

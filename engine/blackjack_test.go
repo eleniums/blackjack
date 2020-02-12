bec8ef2df250a7fcb0ab71ed28ef1cc3bbba08e6
eleniums <eleniums@yahoo.com>
2020-02-12 15:16:38 -0800

    Add tests for determineWinner

--- engine/blackjack_test.go
+++ engine/blackjack_test.go
@@ -3,6 +3,8 @@ package engine
 import (
 	"testing"
 
+	"github.com/eleniums/blackjack/game"
+
 	assert "github.com/stretchr/testify/require"
 )
 
@@ -33,5 +35,146 @@ func Test_Unit_Blackjack_NewBlackjack(t *testing.T) {
 
 	for i, v := range blackjack.players {
 		assert.Equal(t, players[i], v)
+	}
+}
+
+func Test_Unit_Blackjack_determineWinner(t *testing.T) {
+	testCases := []struct {
+		description string
+		player      *Player
+		hand        *game.Hand
+		dealerTotal int
+		win         int
+		loss        int
+		tie         int
+		money       float64
+	}{
+		{
+			description: "Surrendered",
+			player:      &Player{},
+			hand: &game.Hand{
+				Bet:         10,
+				Surrendered: true,
+			},
+			dealerTotal: 1,
+			win:         0,
+			loss:        1,
+			tie:         0,
+			money:       -10,
+		},
+		{
+			description: "Natural_Blackjack",
+			player:      &Player{},
+			hand: &game.Hand{
+				Bet: 10,
+				Cards: []game.Card{
+					game.NewCard(game.SuitClubs, game.RankJack),
+					game.NewCard(game.SuitClubs, game.RankAce),
+				},
+			},
+			dealerTotal: 1,
+			win:         1,
+			loss:        0,
+			tie:         0,
+			money:       15,
+		},
+		{
+			description: "Player_Bust",
+			player:      &Player{},
+			hand: &game.Hand{
+				Bet: 10,
+				Cards: []game.Card{
+					game.NewCard(game.SuitClubs, 9),
+					game.NewCard(game.SuitClubs, 8),
+					game.NewCard(game.SuitClubs, 5),
+				},
+			},
+			dealerTotal: 1,
+			win:         0,
+			loss:        1,
+			tie:         0,
+			money:       -10,
+		},
+		{
+			description: "Dealer_Bust",
+			player:      &Player{},
+			hand: &game.Hand{
+				Bet: 10,
+				Cards: []game.Card{
+					game.NewCard(game.SuitClubs, 2),
+				},
+			},
+			dealerTotal: 22,
+			win:         1,
+			loss:        0,
+			tie:         0,
+			money:       10,
+		},
+		{
+			description: "Player_Loss",
+			player:      &Player{},
+			hand: &game.Hand{
+				Bet: 10,
+				Cards: []game.Card{
+					game.NewCard(game.SuitClubs, game.RankJack),
+					game.NewCard(game.SuitClubs, game.RankQueen),
+				},
+			},
+			dealerTotal: 21,
+			win:         0,
+			loss:        1,
+			tie:         0,
+			money:       -10,
+		},
+		{
+			description: "Player_Win",
+			player:      &Player{},
+			hand: &game.Hand{
+				Bet: 10,
+				Cards: []game.Card{
+					game.NewCard(game.SuitClubs, 9),
+					game.NewCard(game.SuitClubs, 8),
+					game.NewCard(game.SuitClubs, 4),
+				},
+			},
+			dealerTotal: 20,
+			win:         1,
+			loss:        0,
+			tie:         0,
+			money:       10,
+		},
+		{
+			description: "Push",
+			player:      &Player{},
+			hand: &game.Hand{
+				Bet: 10,
+				Cards: []game.Card{
+					game.NewCard(game.SuitClubs, 9),
+					game.NewCard(game.SuitClubs, 8),
+					game.NewCard(game.SuitClubs, 4),
+				},
+			},
+			dealerTotal: 21,
+			win:         0,
+			loss:        0,
+			tie:         1,
+			money:       0,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.description, func(t *testing.T) {
+			// arrange
+			blackjack := &Blackjack{}
+
+			// act
+			blackjack.determineWinner(tc.player, tc.hand, tc.dealerTotal)
+
+			// assert
+			assert.Equal(t, tc.win, tc.player.Win)
+			assert.Equal(t, tc.loss, tc.player.Loss)
+			assert.Equal(t, tc.tie, tc.player.Tie)
+			assert.Equal(t, tc.money, tc.player.Money)
+		})
 	}
 }

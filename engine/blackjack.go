4c732505349c75152f69281cb5825c6a666d4cb4
eleniums <eleniums@yahoo.com>
2020-03-30 10:36:37 -0700

    Write training data from player actions

--- engine/blackjack.go
+++ engine/blackjack.go
@@ -137,6 +137,7 @@ func (b *Blackjack) playHand(player *Player, hand *game.Hand) bool {
 
 		actions := b.possibleActions(player, hand)
 		action = player.AI.Action(b.dealer.Hand, hand, actions)
+		b.ML.WriteAction(b.dealer.Hand, hand, action)
 		switch action {
 		case game.ActionHit:
 			card := b.dealCard(hand, false)
@@ -149,6 +150,7 @@ func (b *Blackjack) playHand(player *Player, hand *game.Hand) bool {
 			if !hand.CanSplit() {
 				fmt.Println("Splitting is only allowed if the starting hand has two cards with equal rank.")
 				action = game.ActionInvalid
+				b.ML.WriteResult(game.ResultInvalid)
 				continue
 			}
 
@@ -163,10 +165,13 @@ func (b *Blackjack) playHand(player *Player, hand *game.Hand) bool {
 			newCard2 := b.dealCard(splitHand, false)
 			fmt.Printf("%s split their hand.\nOne hand was dealt: %v\nThe other hand was dealt: %v\n", player.Name, newCard1, newCard2)
 
+			b.ML.WriteResult(game.ResultNone)
+
 		case game.ActionDouble:
 			if !hand.CanDouble() {
 				fmt.Println("Doubling down is only allowed on the original two cards.")
 				action = game.ActionInvalid
+				b.ML.WriteResult(game.ResultInvalid)
 				continue
 			}
 			card := b.dealCard(hand, false)
@@ -179,6 +184,7 @@ func (b *Blackjack) playHand(player *Player, hand *game.Hand) bool {
 			if !hand.IsInitialHand() || len(player.SplitHands) > 0 {
 				fmt.Println("Surrendering is only allowed on the original two cards before doubling or splitting.")
 				action = game.ActionInvalid
+				b.ML.WriteResult(game.ResultInvalid)
 				continue
 			}
 			player.Hand.Bet /= 2
@@ -188,9 +194,11 @@ func (b *Blackjack) playHand(player *Player, hand *game.Hand) bool {
 
 		case game.ActionStats:
 			b.displayPlayerStats(player)
+			b.ML.WriteResult(game.ResultInvalid)
 
 		case game.ActionExit:
 			fmt.Println("Goodbye!")
+			b.ML.WriteResult(game.ResultInvalid)
 			os.Exit(0)
 		}
 	}
@@ -238,29 +246,36 @@ func (b *Blackjack) determineWinner(player *Player, hand *game.Hand, dealerTotal
 		fmt.Printf("%s surrendered.\n", player.Name)
 		player.Loss++
 		player.Money -= hand.Bet
+		b.ML.WriteResult(game.ResultLoss)
 	} else if hand.IsNatural() {
 		fmt.Printf("%s has a natural blackjack!\n", player.Name)
 		player.Win++
 		player.Money += hand.Bet * 1.5
+		b.ML.WriteResult(game.ResultWin)
 	} else if playerTotal > 21 {
 		fmt.Printf("%s busted with a total of %d.\n", player.Name, playerTotal)
 		player.Loss++
 		player.Money -= hand.Bet
+		b.ML.WriteResult(game.ResultLoss)
 	} else if dealerTotal > 21 {
 		fmt.Printf("%s wins with %d because dealer busted with a total of %d!\n", player.Name, playerTotal, dealerTotal)
 		player.Win++
 		player.Money += hand.Bet
+		b.ML.WriteResult(game.ResultWin)
 	} else if playerTotal < dealerTotal {
 		fmt.Printf("%s has %d, which loses to dealer's %d.\n", player.Name, playerTotal, dealerTotal)
 		player.Loss++
 		player.Money -= hand.Bet
+		b.ML.WriteResult(game.ResultLoss)
 	} else if playerTotal == dealerTotal {
 		fmt.Printf("Push, %s and dealer both have %d.\n", player.Name, playerTotal)
 		player.Tie++
+		b.ML.WriteResult(game.ResultTie)
 	} else if playerTotal > dealerTotal {
 		fmt.Printf("%s has %d, which beats dealer's %d!\n", player.Name, playerTotal, dealerTotal)
 		player.Win++
 		player.Money += hand.Bet
+		b.ML.WriteResult(game.ResultWin)
 	}
 }
 

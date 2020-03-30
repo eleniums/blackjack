7a51c9f38ad18a962346f457742fb10663e28ff9
eleniums <eleniums@yahoo.com>
2020-03-30 10:36:58 -0700

    Fix training data results so there is one entry per line

--- engine/machine_learning.go
+++ engine/machine_learning.go
@@ -10,7 +10,8 @@ import (
 
 // ML contains methods for generating machine learning training data.
 type ML struct {
-	data *os.File
+	needsResult bool
+	data        *os.File
 }
 
 // NewML will create a new ML instance with an open file for storing training data.
@@ -20,7 +21,8 @@ func NewML(trainingDataFile string) *ML {
 		panic(err)
 	}
 	return &ML{
-		data: data,
+		needsResult: false,
+		data:        data,
 	}
 }
 
@@ -35,6 +37,11 @@ func (m *ML) WriteAction(dealer, player *game.Hand, action game.Action) {
 		return
 	}
 
+	// make sure to not continuously write on the same line
+	if m.needsResult {
+		m.WriteResult(game.ResultNone)
+	}
+
 	// a copy is used so the cards will not stay revealed
 	d := game.NewHand(dealer.Cards...)
 
@@ -43,6 +50,7 @@ func (m *ML) WriteAction(dealer, player *game.Hand, action game.Action) {
 	d.Cards[1].Hidden = false
 
 	m.data.WriteString(fmt.Sprintf("D:%s-P:%s-%v", m.formatHand(d), m.formatHand(player), action))
+	m.needsResult = true
 }
 
 // WriteResult to a file for machine learning training purposes.
@@ -50,7 +58,15 @@ func (m *ML) WriteResult(result game.Result) {
 	if m == nil {
 		return
 	}
+
+	// make sure to not write a result without an action
+	if !m.needsResult {
+		return
+		//m.data.WriteString(game.Action(game.ActionInvalid).String())
+	}
+
 	m.data.WriteString(fmt.Sprintf("_%v\n", result))
+	m.needsResult = false
 }
 
 // formatHand for training data.

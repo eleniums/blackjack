c376d5df62ec807beeff3026fec64cbdf8ff60ed
eleniums <eleniums@yahoo.com>
2020-03-30 13:58:35 -0700

    Simplify record and recorder
    Record contains all formatting and writing. Recorder contains the file handle.

--- machine/record.go
+++ machine/record.go
@@ -8,39 +8,53 @@ import (
 	"github.com/eleniums/blackjack/game"
 )
 
-type record struct {
-	dealer string
-	player string
-	action game.Action
-	result game.Result
+// IncludePlayerName will add the player's name to the training data if true.
+var IncludePlayerName = false
+
+// Record of a single player action.
+type Record struct {
+	Name   string
+	Dealer string
+	Player string
+	Action game.Action
+	Result game.Result
 }
 
-func (r *record) Write(file *os.File) {
-	file.WriteString(fmt.Sprintf("%v_%v,%s,%s\n", r.action, r.result, r.dealer, r.player))
+// NewRecord will create a new record.
+func NewRecord(dealer, player *game.Hand, name string) *Record {
+	return &Record{
+		Name:   name,
+		Dealer: formatHand(dealer),
+		Player: formatHand(player),
+	}
 }
 
-func (r *record) AddDealerHand(hand *game.Hand) {
-	// a copy is used so the cards will not stay revealed
-	local := game.NewHand(hand.Cards...)
-
-	// we need to reveal all the dealer cards
-	local.Cards[0].Hidden = false
-	local.Cards[1].Hidden = false
-
-	r.dealer = r.formatHand(local)
-}
-
-func (r *record) AddPlayerHand(hand *game.Hand) {
-	r.player = r.formatHand(hand)
+// Write record to the given file.
+func (r *Record) Write(file *os.File) {
+	if IncludePlayerName {
+		file.WriteString(fmt.Sprintf("%v_%v,%s,%s,%s\n", r.Action, r.Result, r.Dealer, r.Player, r.Name))
+	} else {
+		file.WriteString(fmt.Sprintf("%v_%v,%s,%s\n", r.Action, r.Result, r.Dealer, r.Player))
+	}
 }
 
 // formatHand will return a formatted hand string.
-func (r *record) formatHand(hand *game.Hand) string {
-	cleaned := strings.TrimSpace(hand.String())
+func formatHand(hand *game.Hand) string {
+	// a copy is used so the cards will not stay revealed
+	local := game.NewHand(hand.Cards...)
+
+	// we need to reveal all the cards
+	for i := range local.Cards {
+		local.Cards[i].Hidden = false
+	}
+
+	// remove suits as they are not needed
+	cleaned := strings.TrimSpace(local.String())
 	cleaned = strings.ReplaceAll(cleaned, "♣", "")
 	cleaned = strings.ReplaceAll(cleaned, "♠", "")
 	cleaned = strings.ReplaceAll(cleaned, "♥", "")
 	cleaned = strings.ReplaceAll(cleaned, "♦", "")
 	cleaned = strings.ReplaceAll(cleaned, "  ", " ")
+
 	return cleaned
 }
--- machine/recorder.go
+++ machine/recorder.go
@@ -2,14 +2,11 @@ package machine
 
 import (
 	"os"
-
-	"github.com/eleniums/blackjack/game"
 )
 
 // Recorder contains methods for recording machine learning training data.
 type Recorder struct {
-	data   *os.File
-	record *record
+	data *os.File
 }
 
 // NewRecorder will create a new Recorder instance with an open file for storing training data.
@@ -19,8 +16,7 @@ func NewRecorder(file string) *Recorder {
 		panic(err)
 	}
 	return &Recorder{
-		data:   data,
-		record: nil,
+		data: data,
 	}
 }
 
@@ -29,31 +25,10 @@ func (r *Recorder) Close() {
 	r.data.Close()
 }
 
-// Start will begin a new record.
-func (r *Recorder) Start(dealer, player *game.Hand, action game.Action) {
-	if r == nil {
+// Write a record to a file.
+func (r *Recorder) Write(record *Record) {
+	if r == nil || record == nil {
 		return
 	}
-
-	// close out any existing record
-	if r.record != nil {
-		r.Write(game.ResultInvalid)
-	}
-
-	r.record = &record{
-		action: action,
-	}
-	r.record.AddDealerHand(dealer)
-	r.record.AddPlayerHand(player)
-}
-
-// Write a completed record to a file.
-func (r *Recorder) Write(result game.Result) {
-	if r == nil || r.record == nil {
-		return
-	}
-
-	r.record.result = result
-	r.record.Write(r.data)
-	r.record = nil
+	record.Write(r.data)
 }

19cefd4bda1d5a1e4d3f2c477423aa647bbfadd8
eleniums <eleniums@yahoo.com>
2020-04-14 23:07:13 -0700

    Use leaves package to get predictions without python
    This involved downgrading to xgboost 0.90, but it uses pure Go now and doesn't rely on a python script. As a result of only loading the model once, it's also extremely fast now.

--- machine/predict.go
+++ machine/predict.go
@@ -1,28 +1,44 @@
 package machine
 
 import (
-	"os/exec"
-	"strconv"
-	"strings"
-
+	"github.com/dmitryikh/leaves"
 	"github.com/eleniums/blackjack/game"
 )
 
+var model *leaves.Ensemble
+
+func init() {
+	var err error
+	model, err = leaves.XGEnsembleFromFile("./machine/model.bin", false)
+	if err != nil {
+		panic(err)
+	}
+}
+
 // Predict will feed a dealer hand and player hand into a model and return the resulting label.
 func Predict(dealer *game.Hand, player *game.Hand, modelFile string, predictScript string) Label {
-	d := strconv.Itoa(ConvertHand(FormatHand(dealer)))
-	p := strconv.Itoa(ConvertHand(FormatHand(player)))
+	d := float64(ConvertHand(FormatHand(dealer)))
+	p := float64(ConvertHand(FormatHand(player)))
 
-	cmd := exec.Command(predictScript, modelFile, d, p)
-	out, err := cmd.CombinedOutput()
+	predictions := make([]float64, model.NOutputGroups())
+	fvals := []float64{d, p}
+	err := model.Predict(fvals, 0, predictions)
 	if err != nil {
 		panic(err)
 	}
 
-	n, err := strconv.Atoi(strings.TrimSpace(string(out)))
-	if err != nil {
-		panic(err)
-	}
+	return Label(max(predictions))
+}
 
-	return Label(n)
+// max will return the index of the highest value in the array.
+func max(values []float64) int {
+	index := 0
+	maxValue := 0.0
+	for i, v := range values {
+		if v > maxValue {
+			index = i
+			maxValue = v
+		}
+	}
+	return index
 }
